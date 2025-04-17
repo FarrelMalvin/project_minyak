b402a79b681b9eb48c85e86069d731a1cdbe1ca6
farrelmalvin@gmail.com <farrelmalvin@gmail.com>
2025-04-17 23:58:23 +0700

    Perbaikan koneksi DB Supabase dan load dari .env

--- go.mod
+++ go.mod
@@ -11,5 +11,12 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/generative-ai-go v0.19.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.4 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,36 +5,51 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"project_minyak/routes"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	var err error
-	DB, err = sql.Open("mysql", "root@tcp(127.0.0.1:3307)/project_minyak")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, dbname)
+	fmt.Println("Connecting to:", dsn)
+
+	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		log.Fatal("Error connecting to database:", err)
 	}
 
-	// Test koneksi ke database
-	err = DB.Ping()
+	err = db.Ping()
 	if err != nil {
 		log.Fatal("Database connection failed:", err)
 	}
 
+	DB = db
 	log.Println("Database initialized successfully")
 }
 
 func main() {
-
 	InitDB()
 
 	r := routes.SetupRoutes(DB)
 
 	port := ":9090"
 	fmt.Println("Server running on port", port)
+
 	log.Fatal(http.ListenAndServe(port, r))
 }
