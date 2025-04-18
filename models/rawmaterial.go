97356f8f139be36ced8440b442c73363df2062b3
farrelmalvin@gmail.com <farrelmalvin@gmail.com>
2025-04-19 00:38:34 +0700

    menambahkan json pada struct

--- models/rawmaterial.go
+++ models/rawmaterial.go
@@ -1,8 +1,8 @@
 package models
 
 type RawMaterial struct {
-	RawMaterialID   uint    `gorm:"primaryKey"`
-	RawMaterialName string  `gorm:"not null"`
-	Price           float64 `gorm:"type:decimal(10,2);not null"`
-	Supplier        string  `gorm:"not null"`
+	RawMaterialID   uint    `gorm:"primaryKey" json:"raw_material_id"`
+	RawMaterialName string  `gorm:"not null" json:"raw_material_name"`
+	Price           float64 `gorm:"type:decimal(10,2);not null" json:"price"`
+	Supplier        string  `gorm:"not null" json:"supplier"`
 }
